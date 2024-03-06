4f119ae78fcf74ca7f0fc6504e6825eb34b97085
Mateusz Kacperski <kacperskimateusz95@gmail.com>
2024-03-06 15:44:28 +0100

    es senza bonus

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    $class = ['name' => 'Mateusz',
+            'surname' => 'Kacperski'];
+
+    return view('home', $class);
 });
